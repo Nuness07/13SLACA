399ac11cd3e2e313f4ffc2ea14615cb789051e8d
Gabriel Nunes Monteiro Ferreira <55860771+Nuness07@users.noreply.github.com>
2020-08-17 21:51:08 -0300

    melhorias do projeto

--- js/script.js
+++ js/script.js
@@ -10,17 +10,56 @@ btnSeeMore.addEventListener('click', function(){
 var contentDiscussion = document.querySelector('.content-discussion');
 var newTopic = document.querySelector('.newTopic');
 var btnNewTopic = document.querySelector('.btnNewTopic');
+var btnNewTopic2 = document.querySelector('.new');
+
 
 btnNewTopic.addEventListener('click', function(){
     contentDiscussion.classList.add('hide');
     newTopic.classList.remove('hide');
 })
 
+btnNewTopic2.addEventListener('click', function(){
+    topicSend.classList.add('hide');
+    newTopic.classList.remove('hide');
+})
 
-var expand = document.querySelector('.expand');
-var topicExpanded = document.querySelector('.topic-expanded');
+// Tópico enviado
+var form = document.querySelector('.topicForm');
+var topicSend = document.querySelector('.topic-send');
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+    topicSend.classList.remove('hide');
+    newTopic.classList.add('hide');
+})
+
+// EXPANDIR TÓPICOS
+var expand = document.querySelector('.expand-1');
+var topicExpanded = document.querySelector('.topic-expanded-1');
 
 expand.addEventListener('click', function(){
     expand.classList.add('hide');
     topicExpanded.classList.remove('hide');
+})
+
+var expandTwo = document.querySelector('.expand-2');
+var topicExpandedTwo = document.querySelector('.topic-expanded-2');
+
+expandTwo.addEventListener('click', function(){
+    expandTwo.classList.add('hide');
+    topicExpandedTwo.classList.remove('hide');
+})
+
+// MENU RESPONSIVO
+
+var mobileMenu = document.querySelector('.mobileMenu');
+var btnMenuOpen = document.querySelector('.btnMenu-open');
+var btnMenuClose = document.querySelector('.btnMenu-close');
+
+btnMenuOpen.addEventListener('click', function(){
+    mobileMenu.classList.add('menu-open');
+})
+
+btnMenuClose.addEventListener('click', function(){
+    mobileMenu.classList.remove('menu-open');
 })
